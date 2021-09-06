2b3757880b0ee2a00fc1a642caf501971bc097fd
choihz <choixhz@gmail.com>
2021-09-07 00:01:02 +0900

    단일 스레드 컨텍스트 사용
    - 항상 코루틴이 특정 스레드 안에서 실행된다는 것을 보장

--- src/main/kotlin/com/choihz/playground/main.kt
+++ src/main/kotlin/com/choihz/playground/main.kt
@@ -1,15 +1,17 @@
 package com.choihz.playground
 
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-	GlobalScope.launch(Dispatchers.Unconfined) {
-		println("Starting in ${Thread.currentThread().name}") // Starting in main
+	val dispatcher = newSingleThreadContext("myThread")
+
+	GlobalScope.launch(dispatcher) {
+		println("Starting in ${Thread.currentThread().name}") // Starting in myThread
 		delay(500)
-		println("Resuming in ${Thread.currentThread().name}") // Resuming in kotlinx.coroutines.DefaultExecutor
+		println("Resuming in ${Thread.currentThread().name}") // Resuming in myThread
 	}.join()
 }
