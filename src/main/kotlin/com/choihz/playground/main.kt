f42e04cd77af799467e2602f865c456850f426a8
choihz <choixhz@gmail.com>
2021-09-07 00:02:43 +0900

    스레드 풀 사용
    - 스레드 풀의 가용 스레드에서 코루틴을 시작 및 재개
    - 런타임이 가용 스레드를 정하고 부하 분산을 위한 방법도 정함

--- src/main/kotlin/com/choihz/playground/main.kt
+++ src/main/kotlin/com/choihz/playground/main.kt
@@ -3,15 +3,19 @@ package com.choihz.playground
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.newFixedThreadPoolContext
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-	val dispatcher = newSingleThreadContext("myThread")
+	val dispatcher = newFixedThreadPoolContext(4, "myPool")
 
 	GlobalScope.launch(dispatcher) {
-		println("Starting in ${Thread.currentThread().name}") // Starting in myThread
+		println("Starting in ${Thread.currentThread().name}") // Starting in myPool-1
 		delay(500)
-		println("Resuming in ${Thread.currentThread().name}") // Resuming in myThread
+		println("Resuming in ${Thread.currentThread().name}") // Resuming in myPool-2
+		delay(500)
+		println("Resuming in ${Thread.currentThread().name}") // Resuming in myPool-1
+		delay(500)
+		println("Resuming in ${Thread.currentThread().name}") // Resuming in myPool-2
 	}.join()
 }
